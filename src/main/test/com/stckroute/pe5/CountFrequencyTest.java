f879082b50be7b7b19063692b00eb2bcc1ca5f0c
satchida <51950388+satchida@users.noreply.github.com>
2019-07-04 17:05:56 +0530

    Update CountFrequencyTest.java

--- src/main/test/com/stckroute/pe5/CountFrequencyTest.java
+++ src/main/test/com/stckroute/pe5/CountFrequencyTest.java
@@ -21,18 +21,21 @@ public class CountFrequencyTest {
         test=null;
     }
 
+    //test cases for integers written in word as input
     @Test
     public void integerTest(){
         String result=test.countWordFrequency("one one , two _ two,, three ,four ,six ,nine, five ,four,  one");
         assertEquals("{nine=1, six=1, four=2, one=3, two=2, three=1, five=1}",result);
     }
 
+    //Test cases for word as input
     @Test
     public void nameTest(){
         String result=test.countWordFrequency("jam name ,,king , name , jam  , king,  king");
         assertEquals("{king=3, jam=2, name=2}",result);
     }
 
+    //Test cases for numbers as input
     @Test
     public void numberTest(){
         String result=test.countWordFrequency("123 456 736 ,,, 5647, 123 , 9, 4= 9");
